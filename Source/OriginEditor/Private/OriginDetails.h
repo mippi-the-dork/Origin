a6fbd7d8d977f3c380297ccb238649396351f706
mippithedork <90996296+mippi-the-dork@users.noreply.github.com>
2026-09-17 01:49:36 -0500

    Delete Source directory

--- Source/OriginEditor/Private/OriginDetails.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "IDetailCustomization.h"
-class FOriginDetails final : public IDetailCustomization
-{
-public:
-    virtual void CustomizeDetails(IDetailLayoutBuilder& Builder) override;
-};
